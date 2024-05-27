359c4e1112840c1102330de69903bcf0d3af3d26
NewSecurityAdvisor <146943220+NewSecurityAdvisor@users.noreply.github.com>
2024-05-27 16:14:53 +0200

    hydra command

--- Tricket/app.py
+++ Tricket/app.py
@@ -4,9 +4,4 @@ if __name__ == "__main__":
     #app.run(debug=True)
     app.run(debug=True, host="0.0.0.0", port=5000)
 
-
-#https://getbootstrap.com/docs/5.3/getting-started/introduction/
-# below see Nav
-#https://getbootstrap.com/docs/5.3/components/navbar/
-#https://getbootstrap.com/docs/5.3/content/tables/
-#https://getbootstrap.com/docs/5.3/components/buttons/
+#hydra -l test -P 10-million-password-list-top-1000000.txt 127.0.0.1:5000 http-get /login
